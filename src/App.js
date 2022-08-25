c2f649326eaa27c87502c10af2e6ecb1b1c4786e
Quanvo2710 <109613117+Quanvo2710@users.noreply.github.com>
2022-08-25 23:47:05 +0700

    done

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import "./App.css";
+import AddTaskForm from "./components/AddTaskForm";
+import Header from "./components/Header";
+import TaskList from "./components/TaskList";
+export default function App() {
+  const [tasks, setTasks] = useState([
+    { id: "task_1", title: "LearnJS fundam", status: 0 },
+    { id: "task_2", title: "Code a Todo List lv2", status: 0 },
+  ]);
+  const [showInComplete, setShowInComplete] = useState(false);
+  const [newTask, setNewTask] = useState("123 ");
+
+  const handleSubmit = (e) => {
+    // e.prevenDefault();
+    if (newTask) {
+      const task = {
+        id: Date.now(),
+        title: newTask,
+        status: 0,
+      };
+      setTasks([...tasks, task]);
+      setNewTask("");
+    }
+  };
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  };
+  const setTaskStatus = (taskId, status) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === taskId) {
+          return { ...task, status: status ? 1 : 0 };
+        }
+        return task;
+      })
+    );
+  };
+
+  const removeTask = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+  return (
+    <div className="container">
+      <Header />
+      <TaskList
+        tasks={tasks}
+        showInComplete={showInComplete}
+        setTaskStatus={setTaskStatus}
+        removeTask={removeTask}
+        setShowInComplete={setShowInComplete}
+      />
+
+      <AddTaskForm
+        handleSubmit={handleSubmit}
+        newTask={newTask}
+        handleInputChange={handleInputChange}
+      />
+    </div>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <h1 className="title">
+      Todo list
+      <span>Get one item done at a time</span>
+    </h1>
+  );
+}
+
+export default Header;
